8f1adedeaba8de3d7509ee4c99b9ee7e6cabb8b7
Denimsun Basumatary <denimbasumatary@protonmail.com>
2026-05-12 15:08:08 +0530

    update base url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'asmtechpower.co.in';
+  const baseUrl = 'https://asmtechpower.co.in';
   return {
     rules: [
       {
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "asmtechpower.co.in";
+  const baseUrl = "https://asmtechpower.co.in";
   return [
     {
       url: baseUrl,
